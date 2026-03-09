2f3e16270edaf15789e804d6e00185bdd8cd77ec
owen_lu <owen_lu@sercomm.com>
2026-03-09 10:09:26 +0800

    RDKBDEV-3369:Fix TR069 repeat M Reboot in ACS reboot long run
    Reason for change:
    Fix the coverity issue in the commit for RDKBDEV-3369
    
    Signed-off-by: Owen Lu <owen_lu@sercomm.com>

--- source-embedded/CcspCwmpSession/ccsp_cwmp_sesso_management.c
+++ source-embedded/CcspCwmpSession/ccsp_cwmp_sesso_management.c
@@ -173,7 +173,7 @@ CcspCwmpsoAddCwmpEvent
                 ERR_CHK(rc);                
                 if((rc == EOK) && (!ind))
                 {
-                    if((!pCcspCwmpEvent->CommandKey && !pCcspCwmpEventExist->CommandKey) ||
+                    if((!pCcspCwmpEvent->CommandKey && !pCcspCwmpEventExist->CommandKey) &&
                         (!strlen(pCcspCwmpEvent->CommandKey) && !strlen(pCcspCwmpEventExist->CommandKey)))
                     {
                         check = TRUE;
